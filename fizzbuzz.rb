c0e3a31832d64b2207348de39ddc561a46841186
kileysel <kiley@selmanhouse.com>
2018-06-05 22:02:37 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+  if number % 3==0 
+    "Fizz"
+    elsif number % 5 ==0 
+    "Buzz"
+  end 
+end
+
+puts fizzbuzz(10)
+puts fizzbuzz(20)
+puts fizzbuzz(5)
+puts fizzbuzz(3)
+puts fizzbuzz(4)
+puts fizzbuzz(15)
+
